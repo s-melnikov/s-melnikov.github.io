07b6d51fd785c7f000c954919f6f1adb5d117464
SMelnikov <test@test.com>
2015-08-13 18:59:41 +0300

    xxx

--- demo/js_lessons/js/snippets.js
+++ demo/js_lessons/js/snippets.js
@@ -215,3 +215,18 @@ function convertIntToShortCode(id) {
 
   return chars[id] + code;
 }
+
+function factorial(n) {
+  return (n != 1) ? n * factorial(n - 1) : 1;
+}
+
+
+function noSleep() {
+  var v = document.createElement('video'),
+    s = document.createElement('source')
+  v.setAttribute('loop', '')
+  s.src = 'data:video/webm;base64,GkXfo0AgQoaBAUL3gQFC8oEEQvOBCEKCQAR3ZWJtQoeBAkKFgQIYU4BnQI0VSalmQCgq17FAAw9CQE2AQAZ3aGFtbXlXQUAGd2hhbW15RIlACECPQAAAAAAAFlSua0AxrkAu14EBY8WBAZyBACK1nEADdW5khkAFVl9WUDglhohAA1ZQOIOBAeBABrCBCLqBCB9DtnVAIueBAKNAHIEAAIAwAQCdASoIAAgAAUAmJaQAA3AA/vz0AAA='
+  s.type = 'video/webm' 
+  v.appendChild(s)
+  v.play()
+}
