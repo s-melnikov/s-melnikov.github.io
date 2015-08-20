b36f6b9dfafc82b16b0a0ab1a5c084200273742f
SMelnikov <test@test.com>
2015-08-20 18:58:33 +0300

    xxx

--- demo/js_lessons/js/snippets.js
+++ demo/js_lessons/js/snippets.js
@@ -230,3 +230,107 @@ function noSleep() {
   v.appendChild(s)
   v.play()
 }
+
+function getDataUri(url, callback) {
+    var image = new Image();
+    image.onload = function () {
+        var canvas = document.createElement('canvas');
+        canvas.width = this.naturalWidth; // or 'width' if you want a special/scaled size
+        canvas.height = this.naturalHeight; // or 'height' if you want a special/scaled size
+        canvas.getContext('2d').drawImage(this, 0, 0);
+        callback(canvas.toDataURL('image/png').replace(/^data:image\/(png|jpg);base64,/, ''));
+    };
+    image.src = url;
+}
+
+// Usage
+/*
+getDataUri('/logo.png', function(dataUri) {
+    // Do whatever you'd like with the Data URI!
+});
+*/
+
+var _get = function get(_x, _x2, _x3) { 
+  var _again = true; 
+  _function: while (_again) { 
+    var object = _x, 
+      property = _x2, 
+      receiver = _x3; 
+    desc = parent = getter = undefined; 
+    _again = false; 
+    if (object === null) 
+        object = Function.prototype; 
+    var desc = Object.getOwnPropertyDescriptor(object, property); 
+    if (desc === undefined) { 
+      var parent = Object.getPrototypeOf(object); 
+      if (parent === null) { 
+        return undefined; 
+      } else { 
+        _x = parent; 
+        _x2 = property; 
+        _x3 = receiver; 
+        _again = true; 
+        continue _function; 
+      } 
+    } else if ('value' in desc) { 
+      return desc.value; 
+    } else { 
+      var getter = desc.get; 
+      if (getter === undefined) { 
+        return undefined; 
+      } 
+      return getter.call(receiver); 
+    } 
+  } 
+};
+
+var _createClass = (function () { 
+  function defineProperties(target, props) { 
+    for (var i = 0; i < props.length; i++) { 
+      var descriptor = props[i]; 
+      descriptor.enumerable = descriptor.enumerable || false; 
+      descriptor.configurable = true; 
+      if ('value' in descriptor) 
+        descriptor.writable = true; 
+      Object.defineProperty(target, descriptor.key, descriptor); 
+    } 
+  } 
+  return function (Constructor, protoProps, staticProps) { 
+    if (protoProps) 
+      defineProperties(Constructor.prototype, protoProps); 
+    if (staticProps) 
+      defineProperties(Constructor, staticProps); 
+    return Constructor; 
+  }; 
+})();
+
+function _inherits(subClass, superClass) { 
+  if (typeof superClass !== 'function' && superClass !== null) { 
+    throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); 
+  } 
+  subClass.prototype = Object.create(superClass && superClass.prototype, { 
+    constructor: { 
+      value: subClass, 
+      enumerable: false, 
+      writable: true, 
+      configurable: true 
+    } 
+  }); 
+  if (superClass) {
+    if (Object.setPrototypeOf) {
+      Object.setPrototypeOf(subClass, superClass)
+    } else {
+      subClass.__proto__ = superClass;
+    }
+  }
+}
+
+function _classCallCheck(instance, Constructor) { 
+  if (!(instance instanceof Constructor)) { 
+    throw new TypeError('Cannot call a class as a function');
+  } 
+}
+
+function toType(obj) {
+  return ({}).toString.call(obj).match(/\s([a-zA-Z]+)/)[1].toLowerCase();
+}
--- demo/radio-2/assets/js/scripts.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  setTimeout(function() {
-    document.querySelector("html").classList.add("load-complete")
-  }, 100)
-});
