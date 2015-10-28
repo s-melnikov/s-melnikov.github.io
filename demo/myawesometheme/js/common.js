405e1fa490d179aebf636c09c4b9b11940a48f12
SMelnikov <test@test.com>
2015-10-28 13:55:03 +0200

    xxx

--- demo/myawesometheme/js/common.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-
-	$('#search').on('blur', function() {
-		$(this)[$(this).val() ? 'addClass' : 'removeClass']('not-empty');
-	});
-
-});
--- demo/version-2/manage/api/sys/configs.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-  'url' => '/git/melnikov5.github.io/theme/manage/api/',
-  'router' => 'index.php',
-  'salt' => 's3cr3t'
-];
