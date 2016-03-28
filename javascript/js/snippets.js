28daa31811b2d494ae7454f02c750d8311bcd510
SMelnikov <test@test.com>
2016-03-28 17:24:03 +0300

    xxx

--- javascript/js/snippets.js
+++ javascript/js/snippets.js
@@ -429,3 +429,19 @@ function parseUrl(url) {
   })
   return result;
 }
+
+function leftpad (str, len, ch) {
+  str = String(str);
+
+  var i = -1;
+
+  if (!ch && ch !== 0) ch = ' ';
+
+  len = len - str.length;
+
+  while (++i < len) {
+    str = ch + str;
+  }
+
+  return str;
+}
