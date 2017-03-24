661c728dfb18f51398973ac848d164c1ef27f1b4
s-melnikov <mlnkv87@gmail.com>
2017-03-24 18:57:15 +0200

    'xxx'

--- todos/script.js
+++ todos/script.js
@@ -1,29 +1,38 @@
 const { h, app } = hyperapp
 
-function LayoutView(model, actions) {
-  return h('div', { 'class': 'container' },
+const LayoutView = (model, actions) =>
+  h('div', {
+      'class': 'container'
+    },
     TodosView(model, actions),
     AddTodoView(model, actions)
   )
-}
 
-function TodosView(model, actions) {
-  return h('div', { 'class': 'todos' },
-    model.todos
-      .map((todo, i) => TodoView(todo, i, actions))
+
+const TodosView = (model, actions) =>
+  h('div', {
+      'class': 'todos'
+    },
+    model.todos.map((todo, i) => TodoView(todo, i, actions))
   )
-}
 
-function TodoView(todo, i, actions) {
-  return h('div',
-    { 'class': 'todo' + (todo.completed ? ' completed' : '') },
+const TodoView = (todo, i, actions) =>
+  h('div', {
+      'class': 'todo' + (todo.completed ? ' completed' : '')
+    },
     h('div', {
       'class': 'checkbox',
       onclick: () => actions.toggle(todo)
     }),
     h('div', { 'class': 'text' }, todo.text)
   )
-}
+
+let AddTodoView = (model, actions) =>
+  h('div', {
+      'class': 'add-todo'
+    },
+    h('a', { href: '#' }, '+ Add')
+  )
 
 app({
   model: {
