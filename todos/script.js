8f7054eb032c69d2ef190c8fecd2b4a3e99d29ad
s-melnikov <mlnkv87@gmail.com>
2017-03-27 18:53:14 +0300

    'xxx'

--- todos/script.js
+++ todos/script.js
@@ -1,18 +1,14 @@
 const { h, app } = hyperapp
+const APP_NAME = 'hypertodos'
 
 const LayoutView = (model, actions) =>
-  h('div', {
-      'class': 'container'
-    },
+  h('div', { 'class': 'container' },
     TodosView(model, actions),
     AddTodoView(model, actions)
   )
 
-
 const TodosView = (model, actions) =>
-  h('div', {
-      'class': 'todos'
-    },
+  h('div', { 'class': 'todos' },
     model.todos.map((todo, i) => TodoView(todo, i, actions))
   )
 
@@ -22,35 +18,85 @@ const TodoView = (todo, i, actions) =>
     },
     h('div', {
       'class': 'checkbox',
-      onclick: () => actions.toggle(todo)
+      onclick: () => actions.toggleTodoState(todo)
     }),
-    h('div', { 'class': 'text' }, todo.text)
+    h('div', { 'class': 'text' }, todo.text),
+    h('div', { 'class': 'time' }, fromNow(todo.time))
   )
 
 let AddTodoView = (model, actions) =>
-  h('div', {
-      'class': 'add-todo'
-    },
-    h('a', { href: '#' }, '+ Add')
+  h('div', { 'class': 'add-todo' },
+    model.formShowed ?
+      h('form',
+        {
+          onsubmit: e => {
+            e.preventDefault()
+            actions.addTodo()
+          }
+        },
+        h('input', {
+          required: true,
+          type: 'text',
+          placeholder: 'Type something...',
+          value: model.input,
+          onCreate: el => el.focus(),
+          oninput: ev => actions.setInput(ev.target.value)
+        }),
+        h('button', null, 'Save')
+      ) :
+      h('a', {
+        href: '#',
+        onclick: actions.toggleForm
+      }, '+ Add')
   )
+
+var fromNow = (time, between) => {
+  between = Date.now() / 1000 - Number(time)
+  if (between < 3600) {
+    return ~~(between / 60) + ' m.'
+  } else if (between < 86400) {
+    return ~~(between / 3600) + ' h.'
+  } else {
+    return ~~(between / 86400) + ' d.'
+  }
+}
 
 app({
   model: {
     todos: [
-      { text: "Lorem ipsum dolor sit amet" },
-      { text: "Consectetur adipisicing elit" },
-      { text: "Voluptas consequuntur omnis" },
-      { text: "Quisquam magnam adipisci", completed: true },
-      { text: "Voluptates nostrum optio eos eveniet" }
-    ]
+      { text: "Lorem ipsum dolor sit amet", time: 1490608041 },
+      { text: "Consectetur adipisicing elit", time: 1490608051 },
+      { text: "Voluptas consequuntur omnis", time: 1490608061 },
+      { text: "Quisquam magnam adipisci", time: 1490608071, completed: true },
+      { text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio sunt, error eius temporibus, voluptate illo consectetur dicta mollitia perferendis dolores repellendus numquam? Rem dolor autem earum vitae numquam natus similique.", time: 1490608080 }
+    ],
+    formShowed: false,
+    input: ""
   },
   actions: {
-    toggle: (model, todo) => ({
-      todos: model.todos.map(_todo => {
+    toggleTodoState: (model, todo, actions) => {
+      model.todos.map(_todo => {
         if (_todo === todo) _todo.completed = !_todo.completed
         return _todo
       })
-    })
+      actions.sync()
+      return { todos: model.todos }
+    },
+    toggleForm: model => ({ formShowed: !model.formShowed }),
+    setInput: (_, input) => ({ input }),
+    addTodo: (model, _, actions) => {
+      model.todos.push({
+        text: model.input,
+        time: ~~(Date.now() / 1000)
+      })
+      actions.sync()
+      return { todos: model.todos, input: '', formShowed: false }
+    },
+    addTodos: (_, todos) => ({ todos }),
+    sync: model => { localStorage[APP_NAME] = JSON.stringify(model.todos) }
   },
+  subscriptions: [
+    (_, actions) => actions.addTodos(JSON.parse(localStorage[APP_NAME] || "[]"))
+  ],
   view: LayoutView
 })
