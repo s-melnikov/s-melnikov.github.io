e042e0f0029f5eacfe8698bc15b4d5f024af0772
s-melnikov <mlnkv87@gmail.com>
2017-11-17 17:19:22 +0200

    xxx

---
+++ snippets/js/object_watch.js
@@ -0,0 +1,24 @@
+function observerWatch(obj, prop, fn) {
+  var lastValue = this[prop]
+  Object.defineProperty(obj, prop, {
+    set: function(value) {
+      fn(lastValue, value)
+      lastValue = value
+    },
+    get: function() {
+      return lastValue
+    }
+  })
+}
+
+Object.prototype.watch = function(name, fn) {
+  return observerWatch(this, name,  fn)
+}
+
+user = { name: "user1" }
+user.pass = {
+  pass: "password",
+  salt: "secret"
+}
+
+user.watch('pass', (ldVal, newVal) => { console.log('Set', oldVal, newVal) })
