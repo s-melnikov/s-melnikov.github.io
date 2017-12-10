ba09cebeeec0db719505a4b0040a2da3544270c5
Sergey <mlnkv87@gmail.com>
2017-12-10 21:36:48 +0200

    ...

--- jwt_example/server.php
+++ jwt_example/server.php
@@ -1,2 +1,11 @@
 <?php
 
+if ($token = getallheaders()["Authorization"]) {
+
+} else {
+  if ($request_body = file_get_contents("php://input")) {
+    if ($request_body = json_decode($request_body)) {
+      if (isset())
+    }
+  }
+}
