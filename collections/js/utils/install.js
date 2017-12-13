f4040d8acb56b3682b90ac882c269e040c387209
Sergey <mlnkv87@gmail.com>
2017-12-13 19:51:49 +0200

    ...

--- collections/js/utils/install.js
+++ collections/js/utils/install.js
@@ -1,21 +1,13 @@
 if (location.search.indexOf("install") != -1) {
-
-  let db = Database("test"), i = 0
-  let schema = db.exists("schema") ? db.table("schema") : db.create("schema")
-  fetch("mocks/schema.json").then(resp => resp.json()).then(data => {
-    schema.truncate()
-    data.map(t_scema => {
-      t_scema.uid = t_scema.slug
-      schema.add(t_scema)
-      fetch("mocks/" + t_scema.slug + ".json").then(resp => resp.json()).then(data => {
-        let table = db.exists(t_scema.slug) ? db.table(t_scema.slug) : db.create(t_scema.slug)
-        table.truncate()
-        data.map(item => { table.add(item) })
-        i++
-        if (i == 4) {
-          location.replace(location.href.split("?")[0])
-        }
+  new Storage("my_app").drop()
+  fetch("install/dump.json").then(resp => resp.json()).then(data => {
+    let storage = new Storage("my_app")
+    Object.keys(data).forEach(table_name => {
+      let table = storage.table(table_name)
+      data[table_name].forEach(item => {
+        table.push(item)
       })
     })
+    location.href = location.href.split("?")[0]
   })
 }
