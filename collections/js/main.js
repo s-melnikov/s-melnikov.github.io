acd64dfee7e4c49a614f0436064a71a0e151feec
Sergey <mlnkv87@gmail.com>
2017-12-15 23:43:04 +0200

    ...

--- collections/js/main.js
+++ collections/js/main.js
@@ -1,40 +1,56 @@
 let { h, app } = hyperapp
-
 let storage = new Storage("my_app")
 
 let model = {
   state: {
-    route: location.hash.slice(3)
+    route: location.hash.slice(3),
+    user: { first_name: "Jonh", last_name: "Doe" },
+    tables: []
   },
   actions: {
     router: () => () => ({ route: location.hash.slice(3) })
   }
 }
 
-let Layout = ({ state, actions }) => h("main", null, "Layout",
-  h("p", null,
-    h(Link, { to: "/" }, "Home"),
-    " ",
-    h(Link, { to: "/blog" }, "Blog"),
-    " ",
-    h(Link, { to: "/post/1" }, "Post 1"),
-    " ",
-    h(Link, { to: "/post/2" }, "Post 2"),
-    " ",
-    h(Link, { to: "/post/3" }, "Post 3"),
-  ),
-  h("p", null,
+let Layout = ({ state, actions }) => h("div", { "class": "layout" },
+  h(state.user ? Main : SignIn, { state, actions })
+)
+
+let SignIn = ({ state, actions }) => h("div", { "class": "sign-in" }, "Sign In")
+
+let Main = ({ state, actions }) => h("div", {
+    "class": "container",
+    oncreate: (...args) => {
+      return console.log(args)
+      storage.table("tables").find().then(result => {
+        this.setState({ tables: result.toArray() })
+      })
+    }
+  },
+  h("main", null,
     h(Router, { state, actions },
       h(Route, { path: "/", component: Home }),
-      h(Route, { path: "/blog", component: Blog }),
-      h(Route, { path: "/post/:id", component: Post }),
+      h(Route, { path: "/table/:slug", component: Table })
+    )
+  ),
+  h("header", null,
+    h("a", { href: "#" }, "Sign out")
+  ),
+  h("aside", null,
+    h("menu", null,
+      h( "a", { href: "#" }, "Home")
+    ),
+    h("p", null, "Collections"),
+    h("menu", null,
+      state.tables.map(table =>
+        h( "a", { href: "#/table/" + table.slug  }, table.title)
+      )
     )
   )
 )
 
-let Home = ({ state, actions }) => h("main", null, "Home")
-let Blog = ({ state, actions }) => h("main", null, "Blog")
-let Post = ({ state, actions, params }) => h("main", null, "Post " + params.id)
+let Home = ({ state, actions }) => h("p", null, "Home")
+let Table = ({ state, actions }) => h("p", null, "Table")
 
 let { actions } = app(model, Layout, document.body)
 addEventListener("hashchange", () => {
