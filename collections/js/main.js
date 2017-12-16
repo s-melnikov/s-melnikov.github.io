97428fcb667a002f6961dae608d88ce64d954330
Sergey <mlnkv87@gmail.com>
2017-12-16 15:40:35 +0200

    ...

--- collections/js/main.js
+++ collections/js/main.js
@@ -81,13 +81,13 @@ let Home = ({ state, actions }) => {
     h("h3", null, "Home"),
     h("h4", null, "Choose a table"),
     h("div", { class: "row"},
-      state.tables.map(table =>
+      state.tables ? state.tables.map(table =>
         h("div", { class: "col" },
           h("div", { class: "card" },
             Link({ to: "/table/" + table.slug }, table.title)
           )
         )
-      )
+      ) : null
     )
   )
 }
