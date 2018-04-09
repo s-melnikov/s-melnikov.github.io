38ef0ac18102a8a6c66f3f1d9b76e95b4e29fd89
s-melnikov <mlnkv87@gmail.com>
2018-04-09 17:08:20 +0300

    ...

--- collections/js/store.js
+++ collections/js/store.js
@@ -1,67 +1,48 @@
 const createStore = (reducer, initialState = {}) => {
-  let state = initialState
-  let subscribers = []
+  let state = initialState;
+  let subscribers = [];
   return {
     subscribe(listener) {
-      subscribers.push(listener)
-      return () => subscribers = subscribers.slice(subscribers.indexOf(listener) + 1, 1)
+      subscribers.push(listener);
+      return () => subscribers = subscribers.slice(subscribers.indexOf(listener) + 1, 1);
     },
     dispatch(action) {
-      state = reducer(state, action)
-      subscribers.forEach(subscriber => subscriber(state, action))
-      return action
+      state = reducer(state, action);
+      subscribers.forEach(subscriber => subscriber(state, action));
+      return action;
     },
     getState() {
-      return state
+      return state;
     },
     setState(newSatet) {
-      state = newState
+      state = newState;
     }
   }
 }
 
-
 let initialState = {
-  name: "Conor",
-  redux: "awesome"
-}
+  users: [
+    { id: 1, name: "Ivan Dorn" },
+    { id: 2, name: "John Dave" },
+    { id: 3, name: "Davi Juce" }
+  ]
+};
+
+let actions = {
+  ADD_USER: ({ user }, { users }) => {
+    users.push(action.user);
+    return { users };
+  }
+};
 
 let myReducer = (state, action) => {
-  let newState = Object.assign({}, state)
-  switch (action.type) {
-    case "CHANGE_NAME":
-      newState.name = action.name
-      break
-    case "CHANGE_REDUX_ADJECTIVE":
-      newState.redux = action.adjective
-      break
-    default:
-      // do nothing
+  let newState = Object.assign({}, state);
+  if (actions[action.type]) {
+    Object.assign(newState, actions[action.type](action, newState));
   }
-  return newState
+  return newState;
 }
 
-const store = createStore(myReducer, initialState)
+const store = createStore(myReducer, initialState);
 
-console.log(store.getState(), "initial state")
 
-let unsubscribe = store.subscribe(() => {
-  console.log("i'll console state changes twice then unsubscribe so you will not be notified of the third dispatch", store.getState())
-})
-
-store.dispatch({
-  type: "CHANGE_NAME",
-  name: "Conor Hastings"
-})
-
-store.dispatch({
-  type: "CHANGE_REDUX_ADJECTIVE",
-  adjective: "great"
-})
-
-unsubscribe()
-
-store.dispatch({
-  type: "CHANGE_NAME",
-  name: "Conor Cool Guy"
-})
