66e4293251cfb25100958b5db7bc9e50762654cd
Sergey <mlnkv87@gmail.com>
2018-04-14 12:49:01 +0300

    ...

--- crm/app.js
+++ crm/app.js
@@ -5,50 +5,81 @@ let state = {};
 let actions = {};
 
 let routes = {
-  "/": IndexView
+  "/": IndexView,
+  "/second": SecondView,
+  "/third": ThirdView,
 };
 
 Router(app)(state, actions, routes, document.body);
 
 function Router(app) {
-  return (state, actions, routes, container) => {
-    let router = createRouter(routes);
-    state.route = location.hash;
-    actions.setRoute = route => ({ route });
-    function view(state, actions) {
-      return h("main", null, router(state, actions));
-    }
-    let main = app(state, actions, view, container);
-    window.addEventListener("hashchange", () => main.setRoute(location.hash));
-    return main;
-  }
-  function createRouter(routes) {
-    routes = Object.keys(routes).map(path => {
+  return (state, actions, routes, container) => {    
+    let currentView = null;
+    let hash = () => location.hash.slice(2);
+    let preparedRoutes = Object.keys(routes).map(path => {
       let keys = [];
       let regex = RegExp(path === "*" ? ".*" :
         "^" + path.replace(/:([\w]+)/g, function(_, key) {
           keys.push(key.toLowerCase());
           return "([-\\.%\\w\\(\\)]+)";
         }) + "$");
-      return { regex, keys, component: routes[path] };
-    })
-    return (state, actions) => {
-      return routes.map(({ regex, keys, component }) => {
-        let route = state.route || "/"
-        let match, params = {}
-        if (match = regex.exec(route)) {
-          keys.map((key, i) => params[key] = (match[i + 1] || "").toLowerCase())
-          return component(state, actions, params)
+      return { regex, keys, view: routes[path] };
+    });
+    actions.setRoute = path => { 
+      if (!path || path.type) {
+        path = location.hash.slice(2) || "/";
+      }
+      console.log(path)
+      let match, params = {};
+      preparedRoutes.map(({ regex, keys, view }) => {
+        if (match = regex.exec(path)) {
+          keys.map((key, i) => params[key] = (match[i + 1] || "").toLowerCase());
+          currentView = view;         
         }
-        return null
-      })
-    }
+      });
+      return { route: { path, params } }
+    }; 
+    state.route = actions.setRoute().route;
+    let main = app(state, actions, (state, actions) => currentView(state, actions), container);
+    window.addEventListener("hashchange", main.setRoute);
+    return main;
   }
 };
 
-function IndexView(state, actions, params) {
-  console.log(params)
-  return h("div", null, "Index");
+function IndexView(state, actions) {
+  return h("div", null, "Index",
+    h("p", null, 
+      h("a", { href: "#" }, "index"),
+      h("br"),
+      h("a", { href: "#!/second" }, "second"),
+      h("br"),
+      h("a", { href: "#!/third" }, "tihrd"),
+    )
+  );
+}
+
+function SecondView(state, actions) {
+  return h("div", null, "Second",
+    h("p", null, 
+      h("a", { href: "#" }, "index"),
+      h("br"),
+      h("a", { href: "#!/second" }, "second"),
+      h("br"),
+      h("a", { href: "#!/third" }, "tihrd"),
+    )
+  );
+}
+
+function ThirdView(state, actions) {
+  return h("div", null, "Third",
+    h("p", null, 
+      h("a", { href: "#" }, "index"),
+      h("br"),
+      h("a", { href: "#!/second" }, "second"),
+      h("br"),
+      h("a", { href: "#!/third" }, "tihrd"),
+    )
+  );
 }
 
 // if (localStorage.hypercrm) {
