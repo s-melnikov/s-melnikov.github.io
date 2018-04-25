8270fa92ec104a984444d30d134ceb06e03fad18
s-melnikov <mlnkv87@gmail.com>
2018-04-25 18:03:02 +0300

    ...

--- crm/database.js
+++ crm/database.js
@@ -1,5 +1,7 @@
 (function(global) {
 
+  let regexp = /\$parent\.(\w+)/g;
+
   class Database {
     constructor(name) {
       this.name = name;
@@ -20,8 +22,39 @@
     restore(json) {
       localStorage[this.name] = json;
     }
+    _fields(entries, fields) {
+      fields.uid = true;
+      return entries.map(entry => {
+        let result = {};
+        for (let field in fields) {
+          let where = fields[field].where;
+          if (where && type(where) === "Object") {
+            where = Object.assign({}, where);
+            for (let prop in where) {
+              where[prop] = where[prop].replace(regexp, (_, prop) => {
+                return entry[prop];
+              });
+            }
+          }
+          if (type(fields[field]) === "Object") {
+            let collection = this.collection(field);
+            let entries = collection.findSync(where);
+            result[field] = this._fields(entries, fields[field].fields);
+          } else {
+            result[field] = entry[field];
+          }
+        }
+        return result;
+      });
+    }
     query(params) {
-
+      let result = {};
+      for (let prop in params) {
+        let collection = this.collection(prop);
+        let entries = collection.findSync(params[prop].where);
+        result[prop] = this._fields(entries, params[prop].fields);
+      }
+      return result;
     }
   }
 
@@ -51,7 +84,7 @@
     }
     pushMany(entries, callback) {
       let result = entries.map(entry => {
-        entry.key = uniqid();
+        entry.uid = uniqid();
         return entry;
       }).concat();
       delay(() => {
@@ -67,19 +100,23 @@
         if (callback) callback(result[0]);
       })[0];
     }
+    findSync(where) {
+      let entries = [];
+      let collection = this.get();
+      if (typeof where === "string") {
+        where = { uid: where };
+      }
+      collection.forEach(entry => {
+        for (let prop in where) {
+          if (entry[prop] !== where[prop]) return;
+        }
+        entries.push(Object.assign({}, entry));
+      });
+      return entries;
+    }
     find(where) {
       return new Promise((resolve, reject) => {
-        let entries = [];
-        let collection = this.get();
-        if (typeof where === "string") {
-          where = { $key: where };
-        }
-        collection.forEach(entry => {
-          for (let prop in where) {
-            if (entry[prop] !== where[prop]) return;
-          }
-          entries.push(Object.assign({}, entry));
-        });
+        let entries = this._find(where);
         delay(() => resolve(entries));
       });
     }
@@ -88,7 +125,7 @@
         let collection = this.get();
         let i = 0;
         if (typeof where === "string") {
-          where = { $key: where };
+          where = { uid: where };
         }
         collection.forEach(entry => {
           for (let prop in where) {
@@ -107,7 +144,7 @@
         let new_collection = [];
         let i = 0;
         if (typeof where === "string") {
-          where = { $key: where };
+          where = { uid: where };
         }
         collection.forEach(entry => {
           for (let prop in where) {
@@ -144,6 +181,8 @@
   }
 
   let delay = cb => setTimeout(cb, Math.random() * 100);
+
+  let type = o => Object.prototype.toString.call(o).slice(8, -1);
 
   global.database = name => new Database(name);
 
