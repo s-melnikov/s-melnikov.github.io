88261e7551a8ae3a041e11b9a9a915cbb135c75f
s-melnikov <mlnkv87@gmail.com>
2018-05-17 20:44:24 +0300

    ...

--- crm/main.js
+++ crm/main.js
@@ -52,11 +52,9 @@ class PageCompanies extends Component {
     });
   }
   render() {
-    console.log(this.state.companies)
     return h("div", { class: "view" },
-      h("div", { class: "text-right" },
-        h(Link, { class: "btn",
-          to: "/companies/new" }, "new company")
+      h("div", { class: "controls" },
+        h(Link, { to: "/companies/new" }, "new company")
       ),
       this.state.companies ? h("table", null,
         h("thead", null,
@@ -78,6 +76,110 @@ class PageCompanies extends Component {
       ) : h(Loader)
     )
   }
+}
+
+class PageCompany extends Component () {
+  constructor() {
+
+  }
+  componentWillMount() {
+    // getEntries({ name: "companies", where: key });
+    // getEntries({ name: "employees", where: { company: key } });
+    // getEntries({ name: "tasks", where: { company: key } });
+  }
+  render() {
+    return h("div", { class: "view" },
+      this.state.company ? h(Descriptions, {
+        list: [
+          ["", h("div", { style: { textAlign: "right" } }, isEdit ?
+              [
+                h(Link, {
+                  to: "/companies/" + key,
+                  class: "btn",
+                  onclick: event => {
+                    // if (isNew) {
+                    //   event.preventDefault();
+                    //   db.refs.companies.push(company, entry => {
+                    //     location.hash = "!/companies/" + entry.$key;
+                    //   });
+                    //   setEntries({ name: "companies", entries: null });
+                    // } else {
+                    //   db.refs.companies.find(key).then(result => {
+                    //     let entry = result.first();
+                    //     if (entry) entry.update(company);
+                    //   });
+                    // }
+                  },
+                }, "save"),
+                h(Link, {
+                  class: "btn red",
+                  to: "/companies/" + key
+                }, "cancel")
+              ] : [
+                h(Link, {
+                  class: "btn",
+                  to: "/companies/" + key + "/edit"
+                }, "edit"),
+                h(Link, {
+                  class: "btn red",
+                  to: "/companies/" + key + "/delete"
+                }, "delete")
+              ]
+            )],
+          [ "Name",
+            this.state.isEdit ? h("input", {
+              value: this.state.company.name,
+              onchange: event => this.state.company.name = event.target.value
+            }) : this.state.company.name],
+          [ "Industry",
+            this.state.isEdit ? h("input", {
+              value: this.state.company.industry,
+              onchange: event => this.state.company.industry = event.target.value
+            }) : this.state.company.industry],
+          [ "Phone",
+            this.state.isEdit ? h("input", {
+              value: this.state.company.phone,
+              onchange: event => this.state.company.phone = event.target.value
+            }) : this.state.company.phone],
+          [ "Country",
+            this.state.isEdit ? h("input", {
+              value: this.state.company.country,
+              onchange: event => this.state.company.country = event.target.value
+            }) : this.state.company.country],
+          [ "City",
+            this.state.isEdit ? h("input", {
+              value: this.state.company.city,
+              onchange: event => this.state.company.city = event.target.value
+            }) : this.state.company.city],
+          [ "Address",
+            this.state.isEdit ? h("input", {
+              value: this.state.company.address,
+              onchange: event => this.state.company.address = event.target.value
+            }) : this.state.company.address],
+          this.state.isEdit || [ "Emploees", h(ItemsList, { items: employees,
+              iterator: emploee => h("div", null,
+                h(Link, { to: "/employees/" + emploee.$key },
+                  emploee.first_name + " " + emploee.last_name
+                )
+              )
+            })],
+          this.state.isEdit || [ "Tasks", h(ItemsList, { items: tasks,
+              iterator: task => h("div", null, task.content)
+            })],
+        ]
+      }) : h(Loader)
+    )
+  }
+
+
+
+  // let { companies, employees, tasks, route } = state;
+  // let { getEntries, setEntries } = actions;
+  // let { key, action } = route.params;
+  // let isNew = !key;
+  // let company = isNew ? {} : companies ? Object.assign({}, companies[0]) : null;
+  // let isEdit = isNew || action == "edit";
+
 }
 
 render(h(Main), document.body.querySelector("#main"));
