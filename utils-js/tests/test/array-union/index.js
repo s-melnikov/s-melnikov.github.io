b5d760698687521259d4bb5901857ab0e78bcdbd
s-melnikov <mlnkv87@gmail.com>
2018-06-29 16:23:46 +0300

    ...

---
+++ utils-js/tests/test/array-union/index.js
@@ -0,0 +1,26 @@
+var test = require('tape');
+var union = require('../../packages/array-union');
+
+test('union of intersecting arrays doesn\'t include dupliccates', function (t) {
+  t.plan(1);
+  var arr1 = [1, 2, 3, 4, 5];
+  var arr2 = [3, 4, 5, 6, 7];
+  t.deepEqual(union(arr1, arr2), [1, 2, 3, 4, 5, 6, 7]);
+  t.end();
+});
+
+test('union of non-intersecting arrays return all members', function (t) {
+  t.plan(1);
+  var arr1 = [1, 2, 3, 4, 5];
+  var arr2 = [6, 7, 8, 9, 10];
+  t.deepEqual(union(arr1, arr2), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  t.end();
+});
+
+test('does not mutate', function (t) {
+  t.plan(1);
+  var arr1 = [1, 2, 3, 4, 5];
+  var arr2 = [6, 7, 8, 9, 10];
+  t.notEqual(union(arr1, arr2), arr1);
+  t.end();
+});
---
+++ utils-js/tests/test/string-prune/index.js
@@ -0,0 +1,68 @@
+var test = require('tape');
+var prune = require('../../packages/string-prune');
+
+test('string defaulted with default suffix', function (t) {
+  t.plan(2);
+  var str = 'when shall we three meet again';
+  var result = prune(str, 9);
+  t.equal(result, 'when...');
+  result = prune(str, 13);
+  t.equal(result, 'when shall...');
+  t.end();
+});
+
+test('string defaulted with custom suffix', function (t) {
+  t.plan(2);
+  var str = 'when shall we three meet again';
+  var result = prune(str, 16, ' (etc)');
+  t.equal(result, 'when shall (etc)');
+  result = prune(str, 16, '');
+  t.equal(result, 'when shall we');
+  t.end();
+});
+
+test('no length specified', function (t) {
+  t.plan(1);
+  var str = 'when shall we three meet again';
+  var result = prune(str);
+  t.equal(result, 'when shall we three meet again');
+  t.end();
+});
+
+test('truncation length minus suffix length is shorter than first word', function (t) {
+  t.plan(1);
+  var str = 'when shall we three meet again';
+  var result = prune(str, 6);
+  t.equal(result, '...');
+  t.end();
+});
+
+test('string is shorter than truncation length', function (t) {
+  t.plan(2);
+  var str = 'when shall we';
+  var result = prune(str, 25);
+  t.equal(result, 'when shall we');
+  result = prune(str, 20, ' (more)');
+  t.equal(result, 'when shall we');
+  t.end();
+});
+
+test('suffix is greater than or equal to truncation length', function (t) {
+  t.plan(2);
+  var str = 'when shall we three meet again';
+  var result = prune(str, 10, ' (etc etc etc)');
+  t.equal(result, ' (etc etc etc)');
+  result = prune(str, 7, ' (more)');
+  t.equal(result, ' (more)');
+  t.end();
+});
+
+test('suffix is greater than or equal to string length', function (t) {
+  t.plan(2);
+  var str = 'when shall';
+  var result = prune(str, 10, ' very long suffix');
+  t.equal(result, 'when shall');
+  result = prune(str, 17, ' very long suffix');
+  t.equal(result, 'when shall');
+  t.end();
+});
