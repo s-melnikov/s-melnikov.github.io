3c74d4afd9ad0ef6a75f9cca58e45dc9f1d9a800
smelnikov <sergey.melnikov@comodo.com>
2018-10-08 17:35:43 +0300

    ...

--- timetracker/tasks/scripts/main.js
+++ timetracker/tasks/scripts/main.js
@@ -1,2 +1,5 @@
-import { h, app } from "./hyperapp.js";
-import storage from "./storage.js"
+import { h, app } from './hyperapp.js';
+import storage from './storage.js';
+import state from './state.js';
+import actions from './actions.js';
+import view from './view';
