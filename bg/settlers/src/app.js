11f7a7ce6b7c8e3a786b36af1d4f346cdceb5df4
Serhii Melnykov <mlnkv87@gmail.com>
2026-01-30 20:59:48 +0100

    Cleanup

--- bg/settlers/src/app.js
+++
@@ -1,6 +0,0 @@
-import Game from "./Game.js";
-
-const game = new Game();
-game.start();
-
-console.log(game);
--- timetracker/beta/src/index.js
+++
@@ -1,6 +0,0 @@
-def(['router', 'actions', 'view'], ({ location }, actions, view) => {
-  const { app } = hyperapp;
-  const main = app({ location: location.state }, actions, view, document.body);
-  location.subscribe(main.location);
-  // main.init();
-});
